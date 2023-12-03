d5215cb8e063a62d9dd6cd4d7f818f182056abec
CloudBetter <150063402+cloud-better@users.noreply.github.com>
2023-12-03 22:16:33 +0530

    Create Main.java

---
+++ sample-java-code/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("We are learning Maven!"); 
+    }
+}
+
+
+# Save it as Main.java
+
+# Execute: javac Main.java
+
+# Execute: java Main
